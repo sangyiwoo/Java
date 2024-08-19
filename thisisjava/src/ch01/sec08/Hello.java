052eec6f6667620e8d6f42db72b03798db3de547
sangyi <138323770+woooooosangyi@users.noreply.github.com>
2024-08-19 15:37:25 +0900

    자바 시작

---
+++ thisisjava/src/ch01/sec08/Hello.java
@@ -0,0 +1,10 @@
+package ch01.sec08; //패키지 선언
+
+public class Hello { //클래스 선언
+
+	public static void main(String[] args) { //main() 메소드(프로그램 실행 진입점)
+		System.out.println("Hello, Java");
+
+	}
+
+}
---
+++ thisisjava/src/ch01/sec10/Hello.java
@@ -0,0 +1,14 @@
+package ch01.sec10;
+/**
+ *  우웩웩
+ */
+/*
+장제목: 1장 자바 시작하기
+작성일: 2024.08.16
+ */
+public class Hello {
+ //프로그램 실행 진입점
+	public static void main(String[] args) {
+		System.out.println("Hello, Java");
+	}
+}
